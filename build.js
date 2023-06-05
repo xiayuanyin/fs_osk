20c59668b4ce7e206b176b3d4358c5a1d9ead217
Eli Wang <xinye.wang@foresight-soft.com>
2023-06-05 09:57:02 +0800

    调整构建脚本，支持serve和build两种情况。此外eslint忽略build.js

--- build.js
+++ build.js
@@ -3,7 +3,9 @@ const StylePlugin = require('esbuild-style-plugin')
 
 const fs = require('fs')
 
-const outdir = 'build/debug'
+const mode = process.argv[2] === 'serve' ? 'serve' : 'build'
+
+const outdir = mode === 'serve' ? 'build/debug' : 'build/release'
 
 fs.mkdirSync(outdir, { recursive: true })
 fs.copyFileSync('./playground/index.html', `${outdir}/index.html`)
@@ -11,6 +13,7 @@ fs.copyFileSync('./playground/index.html', `${outdir}/index.html`)
 esbuild.context({
   bundle: true,
   sourcemap: true,
+  minify: mode === 'build',
   entryPoints: ["./playground/index.tsx"],
   outdir,
   plugins: [
@@ -18,8 +21,23 @@ esbuild.context({
       sourceMap: true,
     })
   ]
-}).then((ctx) => ctx.serve({
-  servedir: outdir
-})).then((result) => {
-  console.log('开发服务器已经启动', result)
+}).then((ctx) => {
+  if (mode === 'serve') {
+    ctx.serve({
+      servedir: outdir
+    }).then((result) => {
+      console.log('开发服务器已经启动', result)
+    })
+  } else {
+    ctx.rebuild().then((result) => {
+      if (!result.errors || result.errors.length === 0) {
+        console.log('构建完毕。输出目录：', outdir)
+        process.exit(0)
+      } else {
+        console.error('构建错误')
+        console.error(result.errors)
+        process.exit(1)
+      }
+    })
+  }
 })
